09a19e0241c54d99dd9e1142470098744d979884
Rodrigo <rodrigosbombieri@gmail.com>
2024-01-02 21:29:21 -0300

    DataBase SQL de pokedex y discos

---
+++ dataBase/POKEMON_DB.sql
@@ -0,0 +1,3 @@
+Select * From POKEMONS
+
+update POKEMONS set UrlImagen = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png' where Id = 5
---
+++ dataBase/SQLQuery.sql
@@ -0,0 +1,8 @@
+
+SET IDENTITY_INSERT DISCOS ON
+
+
+Select * From DISCOS
+
+
+update DISCOS set FechaLanzamiento = '2004-02-08 00:00:00', UrlImagenTapa = 'https://www.cmtv.com.ar/tapas-cd/mirandasinrestricciones.jpg' where Id = 10
